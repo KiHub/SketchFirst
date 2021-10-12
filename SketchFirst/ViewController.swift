5f79757bc18dfb868c034ed700afaf160b6be436
Kirill Ki <kirillkalapov@gmail.com>
2021-10-12 16:52:58 +0200

    Initial Commit

---
+++ SketchFirst/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SketchFirst
+//
+//  Created by  Mr.Ki on 12.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
